6f3e00a79204bc5eb99752a7a6033fa0eca76be8
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-10-02 15:24:39 -0400

    latest pushes

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::utilities::{ASTNode, Token};
+use crate::utilities::utilities::{ASTNode, Token};
 
 pub struct Parser {
     tokens: Vec<Token>,
